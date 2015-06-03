21477174ee5f669e621a49b43e2a9530a10762fa
wwbhl <baohailong@sina.com>
2015-06-03 16:41:05 +0800

    first commit

---
+++ phoneapps.mk
@@ -0,0 +1,46 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps := BasicDreams \
+		Bluetooth \
+		BluetoothExt \
+		Camera2 \
+		CertInstaller \
+		DocumentsUI \
+		Galaxy4 \
+		HoloSpiralWallpaper \
+		HTMLViewer \
+		KeyChain \
+		LatinIME \
+		LiveWallpapers \
+		MagicSmokeWallpapers \
+		Nfc \
+		PacProcessor \
+		PhaseBeam \
+		PhotoPhase \
+		PhotoTable \
+		PrintSpooler \
+		SamsungServiceMode \
+		Stk \
+		UserDictionaryProvider \
+		VisualizationWallpapers \
+		WAPPushManager \
+		DSPManager \
+		PinyinIME \
+
+private-phone-priv-apps := BackupRestoreConfirmation \
+		DefaultContainerService \
+		DownloadProvider \
+		InputDevices \
+		ExternalStorageProvider \
+		MediaProvider \
+		FusedLocation \
+		PicoTts \
+		ProxyHandler \
+		SettingsProvider \
+		SharedStorageBackup \
+		Shell \
+		Tag \
+		ThemesProvider \
+		VpnDialogs \
+		WallpaperCropper \
